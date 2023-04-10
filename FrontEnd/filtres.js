26865160977c9d04c6bfd050bac60bda686baa80
Milla22 <marti.ludmilla@orange.fr>
2023-04-10 21:36:53 +0200

    modification affichage filtrage

--- FrontEnd/filtres.js
+++ FrontEnd/filtres.js
@@ -9,78 +9,60 @@ async function getWorksAsPromise() {
 
 async function renderWorks(work) {
     let works= await getWorksAsPromise();
-        /*works.forEach((work)=> {
+    function displayWorks(works) {
+        works.forEach((work)=> {
             projets.innerHTML += `<figure class ="figure" id="figure">
             <img src= "${work.imageUrl}" alt="${work.altTxt}"/>
             <figcaption id="figcaption">${work.title}</figcaption>
             </figure>`
-        })*/
-
-        boutons = document.querySelectorAll('.btnFilter')
-        function activeBtn(e) {
-            boutons.forEach(btn => {
-                btn.classList.remove('active');
-            });
-            e.target.classList.add('active');
-        };
-
-        btnTous.addEventListener('click', function(e) {
-            e.preventDefault();
-            const tous = works.filter (work => work.categoryId!= 0)
-            console.log(tous);
-            activeBtn(e);
-            tous.forEach((work)=> {
-                projets.innerHTML += `<figure class ="figure" id="figure">
-                <img src= "${work.imageUrl}" alt="${work.altTxt}"/>
-                <figcaption id="figcaption">${work.title}</figcaption>
-                </figure>`
-                console.log(work);
-            })
         })
+    }
+    displayWorks(works);
 
-        btnObjets.addEventListener('click', function(e) {
-            e.preventDefault();
-            const objets = works.filter (work => work.categoryId==1)
-            console.log(objets);
-            activeBtn(e);
-            objets.forEach((work)=> {
-                projets.innerHTML += `<figure class ="figure" id="figure">
-                <img src= "${work.imageUrl}" alt="${work.altTxt}"/>
-                <figcaption id="figcaption">${work.title}</figcaption>
-                </figure>`
-                console.log(work);
-            })
-        })
+    boutons = document.querySelectorAll('.btnFilter')
+    function activeBtn(e) {
+        boutons.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        e.target.classList.add('active');
+    };
 
-        btnAppartements.addEventListener('click', function(e) {
+    btnTous.addEventListener('click',function(e) {
         e.preventDefault();
-        const appartements = works.filter (work => work.categoryId==2)
+        let tous = works.filter (work => work.categoryId!= 0)
+        console.log(tous);
+        activeBtn(e);
+        displayWorks(tous) 
+    });
+    
+
+    btnObjets.addEventListener('click', function(e) {
+        e.preventDefault();
+        let objets = works.filter (work => work.categoryId==1)
+        console.log(objets);
+        activeBtn(e);
+        displayWorks(objets) 
+    })
+
+
+    btnAppartements.addEventListener('click', function(e) {
+        e.preventDefault();
+        let appartements = works.filter (work => work.categoryId==2)
         console.log(appartements);
         activeBtn(e);
-        appartements.forEach((work)=> {
-            projets.innerHTML += `<figure class ="figure" id="figure">
-            <img src= "${work.imageUrl}" alt="${work.altTxt}"/>
-            <figcaption id="figcaption">${work.title}</figcaption>
-            </figure>`
-            console.log(work);
-        })
+        displayWorks(appartements);
     })
     
     btnRestaurants.addEventListener('click', function(e) {
         e.preventDefault();
-        const restaurants = works.filter (work => work.categoryId==3)
+        let restaurants = works.filter (work => work.categoryId==3)
         console.log(restaurants);
         activeBtn(e);
-        restaurants.forEach((work)=> {
-            projets.innerHTML += `<figure class ="figure" id="figure">
-            <img src= "${work.imageUrl}" alt="${work.altTxt}"/>
-            <figcaption id="figcaption">${work.title}</figcaption>
-            </figure>`
-            console.log(work);
-        })
+        displayWorks(restaurants);
     })
 }
 renderWorks();
+
 
 
 
