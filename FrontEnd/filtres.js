f9359cc42a973b3057e2054e06230660463623d6
Milla22 <marti.ludmilla@orange.fr>
2023-04-11 10:21:06 +0200

    function delete au filtrage avec anomalie NaN non résolue

--- FrontEnd/filtres.js
+++ FrontEnd/filtres.js
@@ -1,12 +1,12 @@
 
 
-//////////////////////////////////////////////////////////// Filtre////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////// Filtres ////////////////////////////////////////////////////////////////////////
 async function getWorksAsPromise() {
     var promise = await fetch (urlWorks).then(response => {return response.json()});
 
     return await promise;
 }
-
+///////////////////////////////////////////////////////// Affichage des travaux ///////////////////////////////////////////////////////////
 async function renderWorks(work) {
     let works= await getWorksAsPromise();
     function displayWorks(works) {
@@ -19,6 +19,17 @@ async function renderWorks(work) {
     }
     displayWorks(works);
 
+ ///////////////////////////////////////////////////// Suppression afficahage travaux au filtrage /////////////////////////////////////////
+
+    function deleteWorks(works) {
+            works.forEach((work)=> {
+            projets.innerHTML -= `<figure class ="figure" id="figure">
+            <img src= "${work.imageUrl}" alt="${work.altTxt}"/>
+            <figcaption id="figcaption">${work.title}</figcaption>
+            </figure>`
+        })
+    }
+/////////////////////////////////////////////////////// function bouton active ///////////////////////////////////////////////////////////////
     boutons = document.querySelectorAll('.btnFilter')
     function activeBtn(e) {
         boutons.forEach(btn => {
@@ -26,27 +37,28 @@ async function renderWorks(work) {
         });
         e.target.classList.add('active');
     };
-
+/////////////////////////////////////// gestion des boutons filtrage tous, objets, appartements, hotels & restaurants ////////////////////////
     btnTous.addEventListener('click',function(e) {
+        deleteWorks(works);
         e.preventDefault();
         let tous = works.filter (work => work.categoryId!= 0)
         console.log(tous);
         activeBtn(e);
-        displayWorks(tous) 
+        displayWorks(tous);
     });
     
-
     btnObjets.addEventListener('click', function(e) {
         e.preventDefault();
+        deleteWorks(works);
         let objets = works.filter (work => work.categoryId==1)
         console.log(objets);
         activeBtn(e);
-        displayWorks(objets) 
+        displayWorks(objets);
     })
-
 
     btnAppartements.addEventListener('click', function(e) {
         e.preventDefault();
+        deleteWorks(works);
         let appartements = works.filter (work => work.categoryId==2)
         console.log(appartements);
         activeBtn(e);
@@ -55,6 +67,7 @@ async function renderWorks(work) {
     
     btnRestaurants.addEventListener('click', function(e) {
         e.preventDefault();
+        deleteWorks(works);
         let restaurants = works.filter (work => work.categoryId==3)
         console.log(restaurants);
         activeBtn(e);
